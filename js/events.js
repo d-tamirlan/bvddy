e3cee7ddf0bf523dfe4a135e2f89fa55e0d2adc0
Tamirlan <d-tamirlan05@yandex.ru>
2017-09-15 17:22:51 +0300

    almost done header

---
+++ js/events.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $("#menu-open").click(function () {
+        $("#main-nav").fadeToggle(200);
+    });
+
+    $("#menu-close").click(function () {
+        $("#main-nav").fadeToggle(200);
+    })
+});
